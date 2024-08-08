c492aee4acfc82aa1d231de909ffb371e5f6bd93
ISABELLE JULIA DA SILVA FONSECA <isabelle.jsfonseca@senacsp.edu.br>
2024-08-08 17:25:10 -0300

    foi implementado a funcionalidade para criar novos registros de telefone a uma pessoa

---
+++ cadastroTelefone/create.php
@@ -0,0 +1,35 @@
+<?php
+
+require('./../config.php');
+
+$metodo = strtoupper($_SERVER['REQUEST_METHOD']);
+
+if ($metodo === 'POST') {
+    $idPessoa = filter_input(INPUT_POST, 'idPessoa', FILTER_VALIDATE_INT);
+    $ddi = filter_input(INPUT_POST, 'ddi');
+    $ddd = filter_input(INPUT_POST, 'ddd');
+    $telefone = filter_input(INPUT_POST, 'telefone');
+
+    if ($idPessoa && $ddi && $ddd && $telefone) {
+        $sql = $pdo->prepare("INSERT INTO telefone (idPessoa, ddi, ddd, telefone) VALUES (:idPessoa, :ddi, :ddd, :telefone)");
+        $sql->bindValue(':idPessoa', $idPessoa);
+        $sql->bindValue(':ddi', $ddi);
+        $sql->bindValue(':ddd', $ddd);
+        $sql->bindValue(':telefone', $telefone);
+        $sql->execute();
+
+        $array['result'] = [
+            'idPessoa' => $idPessoa,
+            'ddi' => $ddi,
+            'ddd' => $ddd,
+            'telefone' => $telefone
+        ];
+
+    } else {
+        $array['error'] = 'Erro: Valores nulos ou inválidos!';
+    }
+} else {
+    $array['error'] = 'Erro: Método inválido - Apenas POST é permitido.';
+}
+
+require('../return.php');
