3cf9d8ec31ce2faa82f791c4952776bc3a75fbf1
ISABELLE JULIA DA SILVA FONSECA <isabelle.jsfonseca@senacsp.edu.br>
2024-08-08 16:52:55 -0300

    Implementado a  rota de update de pessoa com validação de existência e retorno dos dados atualizados

---
+++ cadastroPessoa/update.php
@@ -0,0 +1,43 @@
+<?php
+require('./header.php');
+
+$metodo = strtoupper($_SERVER['REQUEST_METHOD']);
+
+if ($metodo === 'PUT') {
+    parse_str(file_get_contents("php://input"), $put);
+
+    $id = filter_var($put['id'] ?? null, FILTER_VALIDATE_INT);
+    $nome = $put['nome'] ?? null;
+    $endereco = $put['endereco'] ?? null;
+
+    if ($id && $nome && $endereco) {
+        $sql = $pdo->prepare("SELECT * FROM pessoa WHERE idPessoa = :id");
+        $sql->bindValue(":id", $id);
+        $sql->execute();
+
+        if ($sql->rowCount() > 0) {
+            $sql = $pdo->prepare("UPDATE pessoa SET nome = :nome, endereco = :endereco WHERE idPessoa = :id");
+            $sql->bindValue(':id', $id);
+            $sql->bindValue(':nome', $nome);
+            $sql->bindValue(':endereco', $endereco);
+            $sql->execute();
+
+            $array['result'] = [
+                "id" => $id,
+                "nome" => $nome,
+                "endereco" => $endereco
+            ];
+        } else {
+            $array['error'] = 'Erro: Id inexistente!';
+        }
+    } else {
+        $array['error'] = 'Erro: Valores nulos ou inválidos!';
+    }
+} else {
+    $array['error'] = 'Erro: Método inválido - Apenas PUT é permitido.';
+}
+
+require('./../return.php');
+
+
+
