283143603c98854cf73bcaf47106570532de359d
ISABELLE JULIA DA SILVA FONSECA <isabelle.jsfonseca@senacsp.edu.br>
2024-08-14 16:25:47 -0300

    melhorei o código update

--- cadastroPessoa/update.php
+++ cadastroPessoa/update.php
@@ -1,4 +1,5 @@
 <?php
+
 require('./../config.php');
 
 $metodo = strtoupper($_SERVER['REQUEST_METHOD']);
@@ -6,38 +7,68 @@ $metodo = strtoupper($_SERVER['REQUEST_METHOD']);
 if ($metodo === 'PUT') {
     parse_str(file_get_contents("php://input"), $put);
 
-    $id = filter_var($put['id'] ?? null, FILTER_VALIDATE_INT);
-    $nome = $put['nome'] ?? null;
-    $endereco = $put['endereco'] ?? null;
+    $idpessoa = filter_var($put['idpessoa'] ?? null, FILTER_VALIDATE_INT);
+    $nome = filter_var($put['nome'] ?? null, FILTER_SANITIZE_SPECIAL_CHARS);
+    $endereco = filter_var($put['endereco'] ?? null, FILTER_SANITIZE_SPECIAL_CHARS);
 
-    if ($id && $nome && $endereco) {
-        $sql = $pdo->prepare("SELECT * FROM pessoa WHERE idPessoa = :id");
-        $sql->bindValue(":id", $id);
+
+    if ($idpessoa) {
+        $sql = $pdo->prepare("SELECT * FROM pessoa WHERE idpessoa = :idpessoa");
+        $sql->bindValue(":idpessoa", $idpessoa);
         $sql->execute();
 
         if ($sql->rowCount() > 0) {
-            $sql = $pdo->prepare("UPDATE pessoa SET nome = :nome, endereco = :endereco WHERE idPessoa = :id");
-            $sql->bindValue(':id', $id);
-            $sql->bindValue(':nome', $nome);
-            $sql->bindValue(':endereco', $endereco);
-            $sql->execute();
-
-            $array['result'] = [
-                "id" => $id,
-                "nome" => $nome,
-                "endereco" => $endereco
-            ];
+            if ($nome && $endereco) {
+                $sql = $pdo->prepare("UPDATE pessoa SET nome = :nome, endereco = :endereco WHERE idpessoa = :idpessoa");
+                $sql->bindValue(':idpessoa', $idpessoa);
+                $sql->bindValue(':nome', $nome);
+                $sql->bindValue(':endereco', $endereco);
+                $sql->execute();
+                
+                $array = [
+                    "idpessoa" => $idpessoa,
+                    "nome" => $nome,
+                    "endereco" => $endereco
+                ];
+                $array['result'] = 'Item atualizado com sucesso!';
+            } elseif ($nome) {
+                $sql = $pdo->prepare("UPDATE pessoa SET nome = :nome WHERE idpessoa = :idpessoa");
+                $sql->bindValue(':idpessoa', $idpessoa);
+                $sql->bindValue(':nome', $nome);
+                $sql->execute();
+                
+                $array = [
+                    "idpessoa" => $idpessoa,
+                    "nome" => $nome
+                ];
+                $array['result'] = 'Nome atualizado com sucesso!';
+            } elseif ($endereco) {
+                $sql = $pdo->prepare("UPDATE pessoa SET endereco = :endereco WHERE idpessoa = :idpessoa");
+                $sql->bindValue(':idpessoa', $idpessoa);
+                $sql->bindValue(':endereco', $endereco);
+                $sql->execute();
+                
+                $array = [
+                    "idpessoa" => $idpessoa,
+                    "endereco" => $endereco
+                ];
+                $array['result'] = 'Endereço atualizado com sucesso!';
+            } else {
+                $array['error'] = 'Erro: Nenhum parâmetro válido para atualizar!';
+            }
         } else {
-            $array['error'] = 'Erro: Id inexistente!';
+            $array['error'] = 'Erro: ID inexistente!';
         }
     } else {
-        $array['error'] = 'Erro: Valores nulos ou inválidos!';
+        $array['error'] = 'Erro: ID inválido!';
     }
 } else {
     $array['error'] = 'Erro: Método inválido - Apenas PUT é permitido.';
 }
 
 require('./../return.php');
+
+?>
 
 
 
